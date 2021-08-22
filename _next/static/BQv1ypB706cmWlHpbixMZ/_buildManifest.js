c7e72b4dd37c1c5d2852351ebe955794e884901f
AjayACST <35202521+AjayACST@users.noreply.github.com>
2021-08-22 02:37:19 +0000

    Deploying to gh-pages from @ Boombox-Discord/Website-Plasmic@5242fcd7705d297b7f3134272f3ad5361261897f 🚀

---
+++ _next/static/BQv1ypB706cmWlHpbixMZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/893-0003fecbaae0fc972f30.js","static/css/522463a20f3fda3a0c81.css","static/chunks/pages/index-85267331b1d1ceb15a03.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
